70d3f49d8d5a6d935236e7bd5c9945934b119d6d
pattodiablo <pattodiablo@gmail.com>
2022-06-20 16:49:57 -0500

    Update Main.js

--- WebContent/js/Main.js
+++ WebContent/js/Main.js
@@ -1,16 +1,101 @@
-
+var db;
+var tempdata;
 window.onload = function() {
-	var game = new Phaser.Game(1920, 1080, Phaser.AUTO);
 
-	// Add the States your game has.
-	// game.state.add("Boot", Boot);
-	// game.state.add("Menu", Menu);
-	// game.state.add("Preload", Preload);
-	game.musicOption=1;
-	game.finalScore = 0;
-	game.currentLevel = 1;
-	game.state.add("homeScreen", homeScreen);
-	game.state.add("finalScreen", finalScreen);
-	game.state.add("Level", Level);
-	game.state.start("homeScreen");
+	window.indexedDB = window.indexedDB || window.mozIndexedDB ||
+	window.webkitIndexedDB || window.msIndexedDB;
+
+	//prefixes of window.IDB objects
+	window.IDBTransaction = window.IDBTransaction ||
+		window.webkitIDBTransaction || window.msIDBTransaction;
+	window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange ||
+		window.msIDBKeyRange
+
+	if (!window.indexedDB) {
+		window.alert("Please open this game in the latest version of Google Chrome");
+	}
+
+	var request = window.indexedDB.open("CityHeroDB", 1);
+	var dbfirsttime = false;
+
+
+	request.onerror = function (event) {
+		console.log("db not found");
+	};
+
+	request.onsuccess = function (event) {
+		db = request.result;
+	//	console.log("success: " + db);
+		// remove();
+		if (!dbfirsttime) {
+			
+			const objectStore = db.transaction(['gameData'], "readwrite").objectStore('gameData');
+
+			const objectStoreDataRequest = objectStore.get(1);
+
+			objectStoreDataRequest.onsuccess = () => {
+
+				const result = objectStoreDataRequest.result;
+				tempdata = result.data;
+				console.log(tempdata);
+				crearJuego(tempdata);
+			}
+		}
+		else{
+			var tempdata={
+				coins: 30,
+				level: 1,
+				core1: 0,
+				core2: 0,
+				core3: 0,
+				timesDefeated: 0
+			}
+			crearJuego(tempdata);
+		}
+	};
+
+	request.onupgradeneeded = function (event) {
+		var db = event.target.result;
+		if (event.oldVersion < 1) {
+			var objectStore = db.createObjectStore("gameData",{ keyPath: "id", autoIncrement:true});
+		  }
+		objectStore.add({
+			data: {
+				coins: 30,
+				level: 1,
+				core1: 0,
+				core2: 0,
+				core3: 0,
+				timesDefeated: 0
+			}
+		});
+	  dbfirsttime = true;
+	};
+
+
+	function crearJuego(loadingData){
+		var game = new Phaser.Game(1920, 1080, Phaser.AUTO);
+		
+		game.playerCoins = loadingData.coins;
+		game.playerLevel = loadingData.level;
+		game.playerCore1 = loadingData.core1;
+		game.playerCore2 = loadingData.core2;
+		game.playerCore3 = loadingData.core3;
+		game.timesDefeated = loadingData.timesDefeated;
+		game.musicOption=1;
+		game.finalScore = 0;
+		game.currentLevel = 1;
+		game.state.add("homeScreen", homeScreen);
+		game.state.add("finalScreen", finalScreen);
+		game.state.add("Level", Level);
+		game.state.start("homeScreen");
+
+	}
+
+	
+
+	
+
+
+
 };
